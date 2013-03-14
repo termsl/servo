5d9bf91fac29858b7a9d82ccdf6f6756733edb54
termsl <ser@termsl.ru>
2013-03-14 22:23:57 +0400

    start

---
+++ servo.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess, os,logging, tempfile, ConfigParser, sys
+from datetime import datetime
+
+class servo:
+    useNotifySend       = True
+    useNotifySound      = False #no yet
+    repositoryPath      = None
+    repositoryBranch    = 'origin/master'
+    daemonTimeout       = 60 #sec
+
+    def __init__(self):
+        # log init
+        logging.basicConfig(filename = tempfile.gettempdir() + '/servo.log',
+                            level = logging.DEBUG,
+                            format = '%(asctime)s %(levelname)s: %(message)s',
+                            datefmt = '%Y-%m-%d %I:%M:%S')
+        logging.info('Daemon start')
+
+        # read config
+        config = ConfigParser.ConfigParser()
+        configPath = os.path.dirname(__file__) + '/config.cfg'
+        if os.path.exists(configPath):
+            config.read(configPath)
+        else:
+            self.fireNotify('File "config.cfg" does not exist. Daemon stopped.')
+            sys.exit(2)
+
+        # -- repository path
+        if config.has_option('git', 'repository'):
+            self.repositoryPath = config.get('git', 'repository')
+        else:
+            message = 'Is not define repository path in "config.cfg". Daemon stopped.'
+            self.fireNotify(message)
+            logging.error(message)
+            sys.exit(2)
+
+        # -- repository branch
+        if config.has_option('git', 'branch'):
+            self.repositoryBranch = config.get('git', 'branch')
+
+        # -- daemon timeout
+        if config.has_option('daemon', 'timeout'):
+            self.daemonTimeout = config.getint('daemon', 'timeout')
+
+        # -- use notify-send
+        if config.has_option('notify', 'useNotifySend'):
+            self.useNotifySend = config.getboolean('notify', 'useNotifySend')
+
+        # -- use notify-sound
+        if config.has_option('notify', 'useNotifySound'):
+            self.useNotifySound = config.getboolean('notify', 'useNotifySound')
+
+        # start notify
+        self.fireNotify('Start!')
+
+    def fireNotify(self, msg = '', title = 'GitPushNotify'):
+        """
+        Fire notify action
+        """
+        logging.info('Called fireNotify()')
+        pass
+
+    def getSystemIcon(self):
+        """
+        Get system icon path
+        example: notification-power-disconnected
+        """
+        return ''
+
+    def getLastCheckTime(self):
+        """
+        To simplicity, time of last modification of the current file is used as the time of last checking
+        """
+        lastCheckTime = os.path.getmtime(__file__)
+        return datetime.fromtimestamp(lastCheckTime)
+
+    def setLastCheckTime(self, time = None):
+        """
+        Set time of last checking -> touch file
+        """
+        #commands.getoutput('touch ' + __file__)
+        #os.system('touch ' + __file__)
+        subprocess.check_output('touch ' + __file__, shell=True)
+        return self
+
+    def getRepositoryPath(self):
+        """
+        Get repository path
+        """
+        return self.repositoryPath
+
+    def getDaemonTimeout(self):
+        """
+        Get daemon checking timeout
+        """
+        return self.daemonTimeout
+
+    def check(self, lastCheckTime = None, repositoryPath = None):
+        """
+        Get git log as string
+        """
+        logging.info('Called check()')
+        if (not lastCheckTime):
+            lastCheckTime = self.getLastCheckTime()
+
+        if (not repositoryPath):
+            repositoryPath = self.getRepositoryPath()
+
+        #sourceOutput = commands.getoutput(
+        #sourceOutput = os.system(
+        '''
+        sourceOutput = subprocess.check_output(
+            'cd ' + repositoryPath + ' &&'\
+            + ' git fetch' + ' &&'\
+            + ' git whatchanged ' + self.repositoryBranch + ' -10 --date=raw --date-order --pretty=format:"%H %n%cn %n%ce %n%ct %n%s"',
+            shell=True
+        )
+        logging.debug('sourceOutput: %s', sourceOutput)
+        parser = gitParser.GitParser(sourceOutput)
+        listChanges = parser.getChangesList()
+        message = ''
+        countCommits = 0
+
+        logging.info('Count commits: %s', len(listChanges))
+        for item in listChanges:
+            commitTime = datetime.fromtimestamp(int(item['time']))
+            if (commitTime >= lastCheckTime):
+                message += '...\n' + commitTime.strftime('%x %X') + '\n' + item['author'] + ' &lt;' + item['email']\
+                           + '&gt;\n' + item['message'] + '\n'
+                countCommits += 1
+
+        if (countCommits > 0):
+            logging.info('Count new commits: %s', countCommits)
+            message += '...\n%s new commit(s)\n\n' % countCommits
+
+            self.fireNotify(message)
+            self.setLastCheckTime()
+        '''
+        logging.info('End check()')
+        return self
+
+if __name__ == '__main__':
+    c = servo()
+    c.check()
---
+++ servoDaemon.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Anton Fischer <a.fschr@gmail.com>'
+__date__ = '$30.08.2011 23:33:33$'
+
+import sys, time, tempfile
+import daemon, servo
+ 
+class servoDaemon(daemon.Daemon):
+    def run(self):
+        c = servo.servo()
+        while True:
+            c.check()
+            time.sleep(c.getDaemonTimeout())
+
+if __name__ == '__main__':
+    pidFile = tempfile.gettempdir() + '/daemonServo.pid'
+    daemon = servoDaemon(pidFile)
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            print 'Daemon starting..'
+            daemon.start()
+            print 'Daemon started!'
+        elif 'stop' == sys.argv[1]:
+            print 'Daemon stopping..'
+            daemon.stop()
+            print 'Daemon stopped!'
+        elif 'restart' == sys.argv[1]:
+            print 'Daemon restarting..'
+            daemon.restart()
+            print 'Daemon restarted!'
+        else:
+            print 'Unknown command'
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print 'usage: %s start|stop|restart' % sys.argv[0]
+        sys.exit(2)
